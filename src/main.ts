d41cab7c25ced96bad10388e2a8715afbbc5bd37
interns.trimv <maivantri309@gmail.com>
2023-04-17 19:40:43 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule,DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Cats example')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addSecurity("scheme security",{
+    type:'http',
+    name: 'scheme',
+    in: 'header',
+    scheme: 'Bearer',
+    bearerFormat: 'jwt',
+  })
+  .addSecurityRequirements("scheme security")
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
